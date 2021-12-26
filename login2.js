3a1d5598939f9d5ec2f19b82d2ec458d7772e943
Arnab Sircar <arnab.mist.cse@gmail.com>
2021-12-26 22:54:25 +0600

    Tour in Bangladesh

---
+++ login2.js
@@ -0,0 +1,14 @@
+// Quick exercise
+// Working on a login panel from my Bananaplate project
+// http://dribbble.com/iamnbutler/projects/178899-BANANAPLATE
+// $('.error-page').hide(0);
+
+// $('.login-button , .no-access').click(function(){
+//   $('.login').slideUp(500);
+//   $('.error-page').slideDown(1000);
+// });
+
+// $('.try-again').click(function(){
+//   $('.error-page').hide(0);
+//   $('.login').slideDown(1000);
+// });
